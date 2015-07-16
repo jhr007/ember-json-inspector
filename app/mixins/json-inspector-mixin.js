263fabe9e7da596e000b3f46c026adb390b0bdba
John Robinson <john.holland.robinson@gmail.com>
2015-07-16 12:59:26 -0500

    save

--- app/mixins/json-inspector-mixin.js
+++ app/mixins/json-inspector-mixin.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
 
-export default Ember.Mixin.create({
+// export default Ember.Mixin.create({
+//   actions: {
+//     updatePath: function (path) {
+//       // console.log('passed path', path );
+//       console.log('mixin path', this.get('path') );
+//       this.sendAction('updatePath', path);
+//     },
+//   }
+// });
+
+
+export default {
   actions: {
     updatePath: function (path) {
       // console.log('passed path', path );
-      console.log('mixin path', this.get('path') );
+      console.log('mixin updatePath recieved: ' + path + ' in ' + this.get('path') );
       this.sendAction('updatePath', path);
     },
   }
-});
+};
