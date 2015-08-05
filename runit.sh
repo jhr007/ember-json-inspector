891a3ba2c76af4bc1931dbc88f9e81975ef9289c
John Robinson <john.holland.robinson@gmail.com>
2015-08-05 12:38:36 -0500

    Updated to ember-cli@1.13.7

--- runit.sh
+++ runit.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+
 # Copy to /runit.sh
 # Change port add other options
 # Start server with ./runit.sh
 # Profit
 # 
-ember server --port 4400
+
+EM_PORT=4400
+EM_LR_PORT=$(( $EM_PORT + 1 ));
+
+ember server --port ${EM_PORT} --live-reload-port ${EM_LR_PORT}
